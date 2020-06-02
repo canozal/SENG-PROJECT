648eb3695bdde8725255c63198d010ee2c5a6431
Can Ozal <cannozall@gmail.com>
2020-06-03 02:49:00 +0300

    deneme

--- server.js
+++ server.js
@@ -27,25 +27,13 @@ app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
 
 // Express session
-app.use(
-    session({
-      secret: 'secret',
-      resave: true,
-      saveUninitialized: true
-    })
-  );
 
 
 // Connect flash
 app.use(flash());
 
 // Global variables
-app.use(function(req, res, next) {
-  res.locals.success_msg = req.flash('success_msg');
-  res.locals.error_msg = req.flash('error_msg');
-  res.locals.error = req.flash('error');
-  next();
-});
+
 
 
 
